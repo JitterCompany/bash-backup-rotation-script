864b2bc67009ac51e3e2965e5618d67463f34fb4
Szabolcs Balogh <bszabolcs@gmail.com>
2016-09-14 09:36:14 +0300

    Add email to config file

--- backup.sh
+++ backup.sh
@@ -22,18 +22,19 @@ do
                 then
                         TYPE=`echo $line | awk '{print $1}'`
                         HOSTNAME=`echo $line | awk '{print $2}'`
-                        COMPRESSION=`echo $line | awk '{print $3}'`
-                        SQLFR=`echo $line | awk '{print $4}'`
-                        HOST=`echo $line | awk '{print $5}'`
-                        DATABASE=`echo $line | awk '{print $6}'`
-                        USER=`echo $line | awk '{print $7}'`
-                        PASSWORD=`echo $line | awk '{print $8}'`
-                        FILES_FR=`echo $line | awk '{print $9}'`
-                        DIRECTORY=`echo $line | awk '{print $10}'`
-                        DEST_DIR=`echo $line | awk '{print $11}'`
-                        RETENTION_DAY=`echo $line | awk '{print $12}'`
-                        RETENTION_WEEK=`echo $line | awk '{print $13}'`
-                        RETENTION_MONTH=`echo $line | awk '{print $14}'`
+                        EMAIL=`echo $line | awk '{print $3}'`
+                        COMPRESSION=`echo $line | awk '{print $4}'`
+                        SQLFR=`echo $line | awk '{print $5}'`
+                        HOST=`echo $line | awk '{print $6}'`
+                        DATABASE=`echo $line | awk '{print $7}'`
+                        USER=`echo $line | awk '{print $8}'`
+                        PASSWORD=`echo $line | awk '{print $9}'`
+                        FILES_FR=`echo $line | awk '{print $10}'`
+                        DIRECTORY=`echo $line | awk '{print $11}'`
+                        DEST_DIR=`echo $line | awk '{print $12}'`
+                        RETENTION_DAY=`echo $line | awk '{print $13}'`
+                        RETENTION_WEEK=`echo $line | awk '{print $14}'`
+                        RETENTION_MONTH=`echo $line | awk '{print $15}'`
 
                         if [ ! -e "$DIRECTORY" ]; then
                                 mkdir "$DIRECTORY"
@@ -43,15 +44,15 @@ do
                           if [ ! -e "$DEST_DIR" ]; then
                                 mkdir "$DEST_DIR"
                           fi
-                          $SCRIPT --hostname $HOSTNAME --compression $COMPRESSION --sql "$SQLFR" $HOST $USER $PASSWORD $DATABASE --backupdir "$FILES_FR" $DIRECTORY --targetdir "$DEST_DIR" --retention $RETENTION_DAY $RETENTION_WEEK $RETENTION_MONTH
+                          $SCRIPT --hostname $HOSTNAME --email $EMAIL --compression $COMPRESSION --sql "$SQLFR" $HOST $USER $PASSWORD $DATABASE --backupdir "$FILES_FR" $DIRECTORY --targetdir "$DEST_DIR" --retention $RETENTION_DAY $RETENTION_WEEK $RETENTION_MONTH
                         fi
 
                         if  [ "$TYPE" == 'ftp' ]; then
-                          FTP_HOST=`echo $line | awk '{print $15}'`
-                          FTP_PORT=`echo $line | awk '{print $16}'`
-                          FTP_USER=`echo $line | awk '{print $17}'`
-                          FTP_PASSWORD=`echo $line | awk '{print $18}'`
-                          $SCRIPT --hostname $HOSTNAME --compression $COMPRESSION --sql "$SQLFR" $HOST $USER $PASSWORD $DATABASE --backupdir "$FILES_FR" $DIRECTORY --ftp "$FTP_HOST" "$FTP_PORT" "$FTP_USER" "$FTP_PASSWORD" "$DEST_DIR" --retention $RETENTION_DAY $RETENTION_WEEK $RETENTION_MONTH
+                          FTP_HOST=`echo $line | awk '{print $16}'`
+                          FTP_PORT=`echo $line | awk '{print $17}'`
+                          FTP_USER=`echo $line | awk '{print $18}'`
+                          FTP_PASSWORD=`echo $line | awk '{print $19}'`
+                          $SCRIPT --hostname $HOSTNAME --email $EMAIL --compression $COMPRESSION --sql "$SQLFR" $HOST $USER $PASSWORD $DATABASE --backupdir "$FILES_FR" $DIRECTORY --ftp "$FTP_HOST" "$FTP_PORT" "$FTP_USER" "$FTP_PASSWORD" "$DEST_DIR" --retention $RETENTION_DAY $RETENTION_WEEK $RETENTION_MONTH
                         fi
 
                 fi
